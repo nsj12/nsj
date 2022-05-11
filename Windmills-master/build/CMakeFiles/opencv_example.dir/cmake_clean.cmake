fc8caa58e10b22264a6fdaca05580a61b1792d1a
nsj12 <105288395+nsj12@users.noreply.github.com>
2022-05-12 00:36:56 +0800

    Add files via upload

---
+++ Windmills-master/build/CMakeFiles/opencv_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_example.dir/main.cpp.o"
+  "CMakeFiles/opencv_example.dir/main.cpp.o.d"
+  "opencv_example"
+  "opencv_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
